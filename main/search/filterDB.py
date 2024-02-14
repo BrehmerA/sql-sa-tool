80987a0c9ae74bf6d0ff6a04d614a9a392d57d9e
Aron.Brehmer <arbr2100@student.miun.se>
2024-02-14 14:36:17 +0100

    Fix file structure and skeleton for filter search

---
+++ main/search/filterDB.py
@@ -0,0 +1,9 @@
+class FilterDB:
+    """Responsible for filtering list of repositories on projects containing DB drivers.
+        Will work for projects written in Java and Python.
+    """
+    def __init__(self):
+        """The constructor..."""
+        pass
+
+    
--- main/search/search.py
+++ main/search/search.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from ghapi.all import GhApi
 
 
