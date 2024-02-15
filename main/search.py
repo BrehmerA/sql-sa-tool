9810c744db8a1d9d6cd0a5ffb0f057351270591b
Mina Teräs <mite2100@student.miun.se>
2024-02-15 19:33:07 +0100

    implemented extraction of the link header and began working on extracting the content

--- main/search.py
+++ main/search.py
@@ -1,6 +1,7 @@
+import shlex
+import subprocess
 from pathlib import Path
-
-from ghapi.all import GhApi
+from time import sleep
 
 
 class Search:
@@ -10,10 +11,11 @@ class Search:
     """
 
     PATH_TO_TOKEN = Path(__file__).resolve().parent / '.token'
-    MIN_NUMBER_OF_FOLLOWERS = 2
     LANGUAGES = ('Java', 'Python')
+    MIN_NUMBER_OF_FOLLOWERS = 2
     MIN_SIZE = 100 # kB
-    MIN_NUMBER_OF_STARS = 2 # 1000
+    MIN_NUMBER_OF_STARS = 2
+    NUMBER_OF_RESULTS_PER_PAGE = 4 # Max is 100.
     token = None
     api = None
 
@@ -22,18 +24,48 @@ class Search:
         """The constructor..."""
 
         with open(self.PATH_TO_TOKEN) as file:
-            token = file.read()
-
-        self.api = GhApi(token=token)
+            self.token = file.read()
 
 
     def get_all_repositories(self):
-        # 60 requests per hour if unauthenticated. Otherwise 5000 requests/hour.
+        # The requests library doesn't support pagination.
+        # The API wrapper GhApi wasn't working either since it couldn't find all the repositories.
+        # We therefore went with the subprocess approach, to be able to run curl commands in the terminal.
 
-        # We'll have to use a wrapper, called GhApi, for GitHub's REST API, since pagination isn't supported with the requests library.
+        args = shlex.split(f'''curl --include --request GET --url "https://api.github.com/search/repositories?q=language:{self.LANGUAGES[1]}+followers:>{self.MIN_NUMBER_OF_FOLLOWERS}+size:>{self.MIN_SIZE}+stars:>{self.MIN_NUMBER_OF_STARS}&s=stars&o=desc&per_page={self.NUMBER_OF_RESULTS_PER_PAGE}" --header "Accept: application/vnd.github+json" --header "Authorization: Bearer {self.token}"''')
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        stdout, stderr = process.communicate()
+        output = stdout.decode('utf-8')
 
-        repos = self.api.search.repos(q=f'q=followers:>{self.MIN_NUMBER_OF_FOLLOWERS} language:{self.LANGUAGES[1]} size:>{self.MIN_SIZE} stars:>{self.MIN_NUMBER_OF_STARS}', per_page=1, page=1)
-        print(repos)
+        # Extracts the link header.
+        links = output.split('link: ')[1]
+        links = links.split('x-github-api-version-selected: ')[0] # The header following the link header.
+        links = links.split(', ') # Separates the links.
+        urls = {} # For storing the values with their urls.
+        for link in links:
+            link = link.split('>; rel="') # Separates the url and the value.
+            url = link[0][1:] # Removes the initial <.
+            value = link[1].replace('"', '') # Removes the ending ".
+            value = ''.join(c for c in value if ord(c)>31 and ord(c)<126) # Removes tabs and new lines.
+            urls[value] = url # Saves the value with its url.
+        for key, value in urls.items():
+            print(key, value)
+
+        # Extracts the content.
+        output = output.splitlines()
+        start = None
+        for index in range(0, len(output)):
+            line = output[index]
+            if line == '{':
+                start = index
+                break
+        if start is not None:
+            output = output[index:]
+            for line in output: print(line)
+            # TODO Turn into JSON.
+        else:
+            print("The content couldn't be extracted.")
+            exit()
 
         # TODO For each language: For each repo in each page: Save to the DB.
         # TODO Then filter further on minimum number of contributors. Delete repos that don't meet the critera from the DB.
