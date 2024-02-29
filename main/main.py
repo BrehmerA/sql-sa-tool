24e8adcc583b343482254d12e375b0d75515b3d9
Mina Teräs <mite2100@student.miun.se>
2024-02-29 11:57:46 +0100

    some refactoring

--- main/main.py
+++ main/main.py
@@ -16,7 +16,7 @@ class Main:
     max_number_of_contributors = None
 
 
-    def __validate_input(self, message, boolean, error_message):
+    def __validate_input(self, original, message, boolean, error_message):
         while True:
             value = input(message)
             if len(value) > 0:
@@ -24,21 +24,21 @@ class Main:
                     return value
                 print(f'{error_message} Please try again.')
                 continue
-            break
+            return original
 
 
     def __define_search(self):
         print('You will now be presented a number of options to define your search. The value inside the parentheses is the default. Press Enter to select the default value, otherwise input your preferred value and press Enter.')
 
-        self.language = self.__validate_input(f'Language: ({self.language}) ', lambda value: value == 'Java' or value == 'Python', 'Not a valid input.')
-        self.min_number_of_followers = self.__validate_input(f'Minimum number of followers: ({self.min_number_of_followers}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
-        self.max_number_of_followers = self.__validate_input(f'Maximum number of followers: ', lambda value: int(value) > int(self.min_number_of_followers), 'The value has to be greater than the min value.')
-        self.min_size = self.__validate_input(f'Minimum size: ({self.min_size}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
-        self.max_size = self.__validate_input(f'Maximum size: ', lambda value: int(value) > int(self.min_size), 'The value has to be greater than the min value.')
-        self.min_number_of_stars = self.__validate_input(f'Minimum number of stars: ({self.min_number_of_stars}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
-        self.max_number_of_stars = self.__validate_input(f'Maximum number of stars: ', lambda value: int(value) > int(self.min_number_of_stars), 'The value has to be greater than the min value.')
-        self.min_number_of_contributors = self.__validate_input(f'Minimum number of contributors: ({self.min_number_of_contributors}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
-        self.max_number_of_contributors = self.__validate_input(f'Maximum number of contributors: ', lambda value: int(value) > int(self.min_number_of_contributors), 'The value has to be greater than the min value.')
+        self.language = self.__validate_input(self.language, f'Language: ({self.language}) ', lambda value: value == 'Java' or value == 'Python', 'Not a valid input.')
+        self.min_number_of_followers = self.__validate_input(self.min_number_of_followers, f'Minimum number of followers: ({self.min_number_of_followers}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
+        self.max_number_of_followers = self.__validate_input(self.max_number_of_followers, f'Maximum number of followers: ', lambda value: int(value) > int(self.min_number_of_followers), 'The value has to be greater than the min value.')
+        self.min_size = self.__validate_input(self.min_size, f'Minimum size: ({self.min_size}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
+        self.max_size = self.__validate_input(self.max_size, f'Maximum size: ', lambda value: int(value) > int(self.min_size), 'The value has to be greater than the min value.')
+        self.min_number_of_stars = self.__validate_input(self.min_number_of_stars, f'Minimum number of stars: ({self.min_number_of_stars}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
+        self.max_number_of_stars = self.__validate_input(self.max_number_of_stars, f'Maximum number of stars: ', lambda value: int(value) > int(self.min_number_of_stars), 'The value has to be greater than the min value.')
+        self.min_number_of_contributors = self.__validate_input(self.min_number_of_contributors, f'Minimum number of contributors: ({self.min_number_of_contributors}) ', lambda value: int(value) >= 0, 'The value has to be equal to or greater than 0.')
+        self.max_number_of_contributors = self.__validate_input(self.max_number_of_contributors, f'Maximum number of contributors: ', lambda value: int(value) > int(self.min_number_of_contributors), 'The value has to be greater than the min value.')
 
         print("You've selected the following values:", self.language, self.min_number_of_followers, self.max_number_of_followers, self.min_size, self.max_size, self.min_number_of_stars, self.max_number_of_stars, self.min_number_of_contributors, self.max_number_of_contributors)
 
@@ -67,14 +67,19 @@ class Main:
                         sep = '\t'
                 string += f'{value}{sep}'
             print(string)
-        search_id = None
-        while search_id is None:
-            value = input('Please enter the ID of the search you want to be presented: ')
-            if len(value) > 0 and int(value) in search_id_list:
-                search_id = value
+        search_ids = []
+        while len(search_ids) == 0:
+            ids = input('Please enter the IDs of the searches you want to be presented (use a single space as a separator): ').split(' ')
+            ok = True
+            for id in ids:
+                if int(id) not in search_id_list:
+                    ok = False
+                    break
+            if ok:
+                search_ids = ids
             else:
                 print('Not a valid input. Please try again.')
-        print(f"You've selected the following ID: {search_id}.")
+        print(f"You've selected the following {'IDs' if len(search_ids) > 1 else 'ID'}:", *search_ids, sep=' ')
 
         db.close()
 
@@ -83,7 +88,7 @@ class Main:
         """Main method for executing the program."""
 
         self.__define_search()
-        # Search(self.language, self.min_number_of_followers, self.max_number_of_followers, self.min_size, self.max_size, self.min_number_of_stars, self.max_number_of_stars, self.min_number_of_contributors, self.max_number_of_contributors)
+        Search(self.language, self.min_number_of_followers, self.max_number_of_followers, self.min_size, self.max_size, self.min_number_of_stars, self.max_number_of_stars, self.min_number_of_contributors, self.max_number_of_contributors)
 
         self.__select_search()
 
