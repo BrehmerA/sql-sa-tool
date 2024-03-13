d1184608748c80a40776fb369ae95ff37546ca4e
Aron.Brehmer <arbr2100@student.miun.se>
2024-03-13 09:19:07 +0100

    Insert test data into db

--- main/tests/results_test.py
+++ main/tests/results_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import io
 import unittest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Appends the parent dir to the python path.
@@ -8,7 +9,22 @@ from results import Results
 
 
 class ResultsTest(unittest.TestCase):
-    pass
+    """Unit tests for result class"""
+
+    """def test_print_to_screen(self):
+        sys.stdout = sys.__stdout__
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        res = Results((99999,))
+        res.print_to_screen()
+        sys.stdout = sys.__stdout__
+        self.assertTrue(len(capturedOutput.getvalue()!=0))"""
+
+
+    def test_write_to_file(self):
+        res = Results((99999,))
+        res.write_to_file()
+
 
 
 if __name__ == '__main__':
