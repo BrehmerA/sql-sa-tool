d3f73f836ea4d7f0fb88f372a7062e00efeff305
Aron.Brehmer <arbr2100@student.miun.se>
2024-03-13 08:30:04 +0100

    Add test for analyzing

--- main/tests/analysis_test.py
+++ main/tests/analysis_test.py
@@ -1,8 +1,7 @@
 import os
 import sys
 import unittest
-import uuid
-
+from time import sleep
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Appends the parent dir to the python path.
 from database.database import Database
 from analysis import Analysis
@@ -10,6 +9,7 @@ from analysis import Analysis
 
 
 class AnalysisTest(unittest.TestCase):
+    """Test class for analyzing repositories"""
 
     __analysis = Analysis()
 
@@ -19,13 +19,13 @@ class AnalysisTest(unittest.TestCase):
         DB.connect()
         try:
             DB.execute(f'''
-            INSERT INTO repository (id, name, url, number_of_followers, size, number_of_stars, number_of_contributors) VALUES (?, ?, ?, ?, ?, ?, ?)
+            INSERT OR IGNORE INTO repository (id, name, url, number_of_followers, size, number_of_stars, number_of_contributors) VALUES (?, ?, ?, ?, ?, ?, ?)
             ''', (759767437, 'sql-repo-tester', 'https://api.github.com/repos/BrehmerA/sql-repo-tester', 1, 394, 0, 1))
         except:
             pass
         
         try:
-            DB.execute(f'''INSERT INTO search 
+            DB.execute(f'''INSERT OR IGNORE INTO search 
                 (id, date, language, min_number_of_followers, max_number_of_followers, min_number_of_stars, max_number_of_stars, min_number_of_contributors, min_size)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (10001, '2024-03-01', 1, 2, 10, 20, 100, 2, 100))
@@ -33,7 +33,7 @@ class AnalysisTest(unittest.TestCase):
             pass
         
         try:
-            DB.execute(f'''INSERT INTO search 
+            DB.execute(f'''INSERT OR IGNORE INTO search 
                 (id, date, language, min_number_of_followers, max_number_of_followers, min_number_of_stars, max_number_of_stars, min_number_of_contributors, min_size)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             ''', (10002, '2024-03-01', 2, 2, 10, 20, 100, 2, 100))
@@ -41,24 +41,33 @@ class AnalysisTest(unittest.TestCase):
             pass
 
         try:
-            DB.execute('''INSERT INTO search_repository (search, repository) VALUES (?, ?)''', (10001, 759767437))
+            DB.execute('''DELETE from result WHERE search=10001''')
+            DB.execute('''DELETE from search_repository WHERE search=10001''')
+            DB.execute('''INSERT OR IGNORE INTO search_repository (search, repository) VALUES (?, ?)''', (10001, 759767437))
         except:
             pass
         try:
-            DB.execute('''INSERT INTO search_repository (search, repository) VALUES (?, ?)''', (10002, 759767437))
+            DB.execute('''DELETE from result WHERE search=10002''')
+            DB.execute('''DELETE from search_repository WHERE search=10002''')
+            DB.execute('''INSERT OR IGNORE INTO search_repository (search, repository) VALUES (?, ?)''', (10002, 759767437))
         except:
             pass
 
         DB.close()
 
     def test_init(self):
-        self.assertIsNotNone(self.__analysis.DB._Database__CONNECTION)
-        self.assertIsNotNone(self.__analysis.DB._Database__CURSOR)
+        """Test init. DB initialise and folder created correctly"""
         self.assertTrue(os.path.exists(os.getcwd() + '/cloned'))
 
-    def test_getRepos(self):
-        self.assertEqual(self.__analysis.getRepos('Python', 10002), [['https://github.com/BrehmerA/sql-repo-tester.git', 759767437]])
-        self.assertEqual(self.__analysis.getRepos('Java', 10001), [['https://github.com/BrehmerA/sql-repo-tester.git', 759767437]])
+    def test_filter_and_analyse(self):
+        """Test run through with analyzing and writing result to DB"""
+        self.__analysis.startFilter(10001)
+        DB=Database()
+        DB.connect()
+        res = DB.fetch_all('''SELECT * from result WHERE search=10001''')
+        DB.close()
+        self.assertTrue(len(res)==1)
+        
 
 if __name__ == '__main__':
     unittest.main()
